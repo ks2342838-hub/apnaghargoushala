f20781f11d9ff99a53620594bb146c3acee5863d
ks2342838-hub <ks2342838@gmail.com>
2026-06-10 12:44:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple alert if someone tries to submit donation form
+document.getElementById('submitBtn')?.addEventListener('click', function() {
+    alert('विवरण सबमिट करने के लिए धन्यवाद! कृपया ऊपर दिए गए क्यूआर कोड को स्कैन करके भुगतान पूरा करें।');
+});
